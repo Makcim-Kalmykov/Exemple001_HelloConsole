6dc8f9d70f1c7803bce9a2a8f3309aa3a981ed50
Maksim Kalmykov <makc_mail@list.ru>
2022-08-21 20:44:19 +0300

    Add Exemple 004 random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int namberA = 31;
-int namberB = 5;
+﻿int namberA = new Random().Next(1, 10);  //Будет давать случайное число 0, 1, 2, 3....9
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
 int result = namberA + namberB;
 Console.WriteLine(result);
